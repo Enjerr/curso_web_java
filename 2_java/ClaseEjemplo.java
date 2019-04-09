e83f46af63cc6d2fd3c513b907f12b9264e98fce
Enmanuel <ramosrozalen@gmail.com>
2019-04-09 12:13:51 +0200

    Dia 2 9/04/2019

---
+++ 2_java/ClaseEjemplo.java
@@ -0,0 +1,15 @@
+class ClaseEjemplo{
+	public static void main(String[] args){
+		System.out.println("hola Almería" + args[0]);
+		int variableEntera = 10;
+		float varDecimal=30.55555555555f;
+		double varDoblePrecision=30.99999999999999999999999999999999999;
+		String texto= "Cadena de texto";
+		boolean booleano = true;
+		System.out.println("Valor:" + variableEntera);
+		System.out.println("Valor:" + varDecimal);
+		System.out.println("Valor:" + varDoblePrecision);
+		System.out.println("Valor:" + texto);
+		System.out.println("Valor:" + booleano);
+	}
+}
